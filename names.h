3ea74fded3666de713e1dd46f0c45eb9f2a991ff
uptheshipcreek <northenore@gmail.com>
2022-01-18 16:28:32 +0200

    Names of the nodes of attiki odos

---
+++ names.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Names {
+   static const string node_name[];
+};
+
+const string Names::node_name[] = {
+"Olympia Odos", 
+"Mandra", 
+"Magoula",
+"A13",
+"Aspropyrgos",
+"Aigaleo",
+"Fyles",
+"Demokratias",
+"A1",
+"Heraclius",
+"Kymi",
+"Kephisia",
+"Penteles",
+"Doukisses Plakentias",
+"Gerakas",
+"Marathonos",
+"Rafena",
+"Kantza",
+"Paiania",
+"A63",
+"Airport",
+};
